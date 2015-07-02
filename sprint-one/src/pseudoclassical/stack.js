a339b002ba50ac797cf20196c57d1ccb4929339d
hokennethk <hokennethk@gmail.com>
2015-07-01 20:27:19 -0700

    pseudoclassical queue and stack - in progress

--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
+  this.storage = {};
 };
+
+Stack.prototype.push = function(value){
+	this.length ++;
+	this.storage[this.length] = value;
+};
+
+Stack.prototype.pop = function(){
+	if (this.size()){
+		var temp = this.storage[this.length];
+		delete this.storage[this.length];
+		this.length --;
+		return temp;
+	}
+
+};
+
+Stack.prototype.size = function(){
+	return this.length;
+};
+
+
 
 
