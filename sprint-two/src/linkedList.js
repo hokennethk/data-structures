25479d4f5992991fbf1097a672db61273318faa0
hokennethk <hokennethk@gmail.com>
2015-07-02 11:06:56 -0700

    implemented list.addToTail

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -3,8 +3,30 @@ var LinkedList = function(){
   list.head = null;
   list.tail = null;
 
-  list.addToTail = function(value){
-  };
+  list.addToTail = function(value) {
+    //traverse through list until the value of node.next = null
+    //if null - node.next = new node with value
+    //set newNode.next = null
+
+    //if the list is empty, the node passed in is = head
+    var newNode = Node(value);
+    var current = list.head;
+    if (current === null) {
+        //the list is empty
+        list.head = newNode;
+        list.tail = newNode;
+        return;
+    } else {
+        while (current.next !== null) {
+            current = current.next;
+        }
+        //current is the last item in the list
+        current.next = newNode;
+        list.tail = newNode;
+
+    }
+
+};
 
   list.removeHead = function(){
   };
