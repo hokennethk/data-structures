f16709d10ca896e9c90a7967483e65e78210aae4
hokennethk <hokennethk@gmail.com>
2015-07-03 15:43:49 -0700

    Implemented Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,33 @@
 var Set = function(){
   var set = Object.create(setPrototype);
-  set._storage = undefined;
+  set._storage = [];
   return set;
 };
 
 var setPrototype = {};
 
 setPrototype.add = function(item){
+	if (!this.contains(item)) {
+		this._storage.push(item);
+	}
 };
 
 setPrototype.contains = function(item){
+	var contains = false;
+	_.each(this._storage,function(value){
+		if (value === item){
+			contains = true;
+		}
+	});
+
+	return contains;
 };
 
 setPrototype.remove = function(item){
+	if (this.contains(item)){
+		var index = this._storage.indexOf(item);
+		this._storage.splice(index,1);
+	}
 };
 
 /*
